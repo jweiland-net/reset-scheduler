06f16239084848a677be1d302e1a7a0f5afeaa19
Stefan Froemken <sfroemken@jweiland.net>
2025-02-24 17:32:58 +0100

    Remove TYPO3 11 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
-            'scheduler' => '11.5.0-12.4.99',
+            'typo3' => '12.4.25-13.4.99',
+            'scheduler' => '12.4.25-13.4.99',
         ],
         'conflicts' => [
         ],
